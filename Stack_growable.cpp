60dd2dd28acd4604e2cc5744e25e7338635154d6
sahir7 <mohdsahir2762@gmail.com>
2020-07-02 20:04:20 +0530

    exception handling

--- Stack_growable.cpp
+++ Stack_growable.cpp
@@ -1,19 +1,30 @@
 #include<iostream>
 
 using namespace std;
-class Stack{
+
+class Stack{	
 	int l;
 	int in;
 	int *a;
 	public:
-	Stack(){
-		l=1;
-		in=-1;
-		a =new int [l];
-	}
+		Stack(){
+			l=1;
+			in=-1;
+			a =new int [l];
+		}
 	
 	
-	int *make_new(int *a){
+		int *make_new(int *a);
+	      
+		void push( int val);
+	
+		void pop();
+
+		void top();
+		
+};
+
+int* Stack::make_new(int *a){
 		int * new_a=new int [2*l];
 		for(int i=0;i<l;i++){
 			new_a[i]=a[i];
@@ -21,44 +32,55 @@ class Stack{
 		l*=2;
 		return new_a;
 	}
-	void push( int val){
-		cout << "here" << endl;
-		if(in==l-2){
-			cout<<"ksmks";
-			a=make_new(a);
+
+void Stack::push( int val){
+                if(in==l-2){
+                        a=make_new(a);
+                }
+                a[++in]=val;
+        }
+
+
+void Stack:: pop(){
+                
+		try{
+			if(in==-1)
+				throw 1;
+			else
+				in--;
 		}
-		a[++in]=val;
-	}
-
-	void pop(){
-		if(in==-1){
-			cout<<"stack is empty";
+		catch(int e){
+			cout<<"exception caught "<<endl;
 		}
 
-		in--;
-	}
-	void top(){
-		if(in==-1){
-			cout<<"stack is empty";
-		}
-		cout<<a[in]<<endl;
-	}
+                
+        }
 
 
-
-		
-};
+void Stack:: top(){
+                try{
+                        if(in==-1)
+                                throw 1;
+                        else
+                               cout<<a[in]<<endl; 
+                }
+                catch(int e){
+                        cout<<"exception caught "<<endl;
+                }       
+        }
 
 int main(){
 	Stack* s=new Stack();
-	for (int i = 0 ; i < 100;i++)
-	{	cout << "there " << endl;
-	s->push(i);
-	}	
+//	for (int i = 0 ; i < 100;i++)
+//	{	cout << "there " << endl;
+//	s->push(i);
+//	}	
+//	s->top();
+	s->pop();
+	s->push(1);
 	s->top();
 	s->pop();
 	s->pop();
-	s->top();
 
          delete s;
 	return 0;
